b9a0e0937c7ed8769e981a22bc8c15a133c00071
dimitri <dimitri.hurri@gmail.com>
2014-12-02 13:33:42 +0100

    Ajout pagination, recherche par mots clés, page catalogue

---
+++ src/Bdloc/AppBundle/Form/BookSearchType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Bdloc\AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class BookSearchType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title','text', array(
+                    "label" => " "
+                ))
+            ->add('submit', 'submit', array(
+                "label" => "Rechercher"
+            ))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Bdloc\AppBundle\Entity\Book'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'bdloc_appbundle_book';
+    }
+}
