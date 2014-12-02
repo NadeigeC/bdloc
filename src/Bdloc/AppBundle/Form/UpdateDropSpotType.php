785d05841e2d0859ff5dad64ff72764bce85a1ff
nadeige <nadeige.couthon@orange.fr>
2014-12-02 17:05:35 +0100

    ajout de la page profile

---
+++ src/Bdloc/AppBundle/Form/UpdateDropSpotType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Bdloc\AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('email')
+            ->add('password')
+            ->add('token')
+            ->add('salt')
+            ->add('roles')
+            ->add('firstName')
+            ->add('lastName')
+            ->add('zip')
+            ->add('adress')
+            ->add('phone')
+            ->add('isActive')
+            ->add('dateCreated')
+            ->add('dateModified')
+            ->add('dropSpotId')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Bdloc\AppBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'bdloc_appbundle_user';
+    }
+}
---
+++ src/Bdloc/AppBundle/Form/UpdateProfileType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Bdloc\AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('email')
+            ->add('firstName')
+            ->add('lastName')
+            ->add('zip')
+            ->add('adress')
+            ->add('phone')
+            ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Bdloc\AppBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'bdloc_appbundle_user';
+    }
+}
