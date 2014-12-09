999aa202172b00045ff38184f715f1ca3e83832a
nadeige <nadeige.couthon@orange.fr>
2014-12-09 16:30:46 +0100

    coordonnées du point relais sur update ok

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Bdloc\AppBundle\BdlocAppBundle(),
-            new Bazinga\Bundle\GeocoderBundle\BazingaGeocoderBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
