02f2654735ffd3b6fa97bba47a262f9b0e6cdb0a
dimitri <dimitri.hurri@gmail.com>
2014-12-12 10:01:35 +0100

    ajout nb page pagination

--- web/js/app.js
+++ web/js/app.js
@@ -66,7 +66,7 @@ app = {
 	init: function() {
 
 		// Gestion du formulaire en Ajax
-        $("#formfilter").on("submit", "form", this.myCriteres)
+        //$("#formfilter").on("submit", "form", this.myCriteres)
 
         // init pop up
         popup.init()
